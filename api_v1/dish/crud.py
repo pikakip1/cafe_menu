363dbfd7d126f29bab073e1564995cc7f5961d08
pikakip1 <pikakip222@mail.ru>
2024-01-30 18:25:28 +0300

    add router and crud for dish

--- api_v1/dish/crud.py
+++ api_v1/dish/crud.py
@@ -1,18 +1,15 @@
 import uuid
-from typing import Any, Sequence
 
 from fastapi import HTTPException
-from sqlalchemy import select, and_, exists, Row, RowMapping, Result
-from sqlalchemy.exc import NoResultFound
-from sqlalchemy.orm import selectinload, joinedload
-from starlette import status
-from api_v1.sub_menu.crud import get_sub_menu_by_menu_id
-from api_v1.dependencies import get_menu, get_sub_menu
-from src.dish.models import DishORM
-from src.menu.models import MenuORM
-from src.sub_menu.models import SubMenuORM
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from api_v1.sub_menu.schemas import SubMenuCreate, SubMenuPartUpdate
+from sqlalchemy.orm import selectinload
+from starlette import status
+
+from api_v1.dependencies import get_sub_menu_by_menu_id
+from api_v1.dish.schemas import DishCreate, DishPartUpdate
+from src.dish.models import DishORM
+from src.sub_menu.models import SubMenuORM
 
 
 async def get_dishes(
@@ -20,12 +17,78 @@ async def get_dishes(
         submenu_id: uuid.UUID,
         session: AsyncSession
 ) -> list[DishORM] | None:
-
-    sub_menu = await get_sub_menu_by_menu_id(session=session, related_menu_id=menu_id, sub_menu_id=submenu_id)
+    try:
+        sub_menu = await get_sub_menu_by_menu_id(session=session, related_menu_id=menu_id, sub_menu_id=submenu_id)
+    except HTTPException:
+        return []
     result = await session.execute(
-        select(SubMenuORM).
-        options(selectinload(SubMenuORM.dishes))
-        .where(SubMenuORM.id == sub_menu.id)
-    )
-    sub_menu_load = result.scalars().first()
-    return sub_menu_load.dishes
+            select(SubMenuORM).
+            options(selectinload(SubMenuORM.dishes))
+            .where(SubMenuORM.id == sub_menu.id)
+        )
+    sub_menu_with_dishes = result.scalars().first()
+    return sub_menu_with_dishes.dishes
+
+
+async def get_dish(
+        menu_id: uuid.UUID,
+        submenu_id: uuid.UUID,
+        dish_id: uuid.UUID,
+        session: AsyncSession
+) -> DishORM | None:
+    sub_menu = await get_sub_menu_by_menu_id(session=session, related_menu_id=menu_id, sub_menu_id=submenu_id)
+    stmt = (select(DishORM).
+            filter(
+        (DishORM.sub_menu_id == sub_menu.id) &
+        (DishORM.id == dish_id)
+    ))
+    result = await session.execute(stmt)
+    dish = result.scalars().first()
+    if not dish:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='dish not found'
+        )
+    return dish
+
+
+async def create_dish(
+        menu_id: uuid.UUID,
+        submenu_id: uuid.UUID,
+        new_dish_data: DishCreate,
+        session: AsyncSession
+) -> DishORM:
+    sub_menu = await get_sub_menu_by_menu_id(session=session, related_menu_id=menu_id,  sub_menu_id=submenu_id)
+    new_dish = DishORM(**new_dish_data.model_dump())
+    new_dish.sub_menu_id = sub_menu.id
+    session.add(new_dish)
+    await session.commit()
+    return new_dish
+
+
+async def update_dish(
+        menu_id: uuid.UUID,
+        sub_menu_id: uuid.UUID,
+        dish_id: uuid.UUID,
+        update_data: DishPartUpdate,
+        session: AsyncSession
+) -> DishORM:
+    dish = await get_dish(session=session, menu_id=menu_id, submenu_id=sub_menu_id, dish_id=dish_id)
+    for name, value in update_data.model_dump(exclude_unset=True).items():
+        setattr(dish, name, value)
+    await session.commit()
+    await session.refresh(dish)
+    return dish
+
+
+async def delete_dish(
+        menu_id: uuid.UUID,
+        sub_menu_id: uuid.UUID,
+        dish_id: uuid.UUID,
+        session: AsyncSession
+) -> dict:
+    dish = await get_dish(session=session, menu_id=menu_id, submenu_id=sub_menu_id, dish_id=dish_id)
+    dish_title = dish.title
+    await session.delete(dish)
+    await session.commit()
+    return {'Блюдо удалено': dish_title}
--- api_v1/dish/router.py
+++ api_v1/dish/router.py
@@ -1,17 +1,13 @@
 import uuid
-from typing import Type, List
+from typing import List
 
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.dish.schemas import Dish
-from api_v1.sub_menu.dependencies import get_sub_menu
-from src.database import async_db_manager
 from api_v1.dish import crud
-from api_v1.sub_menu.schemas import SubMenu, SubMenuCreate, SubMenuUpdate, SubMenuPartUpdate
+from api_v1.dish.schemas import Dish, DishCreate, DishPartUpdate
+from src.database import async_db_manager
 from src.dish.models import DishORM
-from src.sub_menu.models import SubMenuORM
-
 
 router = APIRouter(tags=['Dish'])
 
@@ -23,3 +19,59 @@ async def get_dishes(
         session: AsyncSession = Depends(async_db_manager.scoped_session_dependency)
 ) -> list[DishORM]:
     return await crud.get_dishes(session=session, menu_id=menu_id, submenu_id=submenu_id)
+
+
+@router.get('/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}', response_model=Dish, status_code=status.HTTP_200_OK)
+async def get_dish(
+        menu_id: uuid.UUID,
+        submenu_id: uuid.UUID,
+        dish_id: uuid.UUID,
+        session: AsyncSession = Depends(async_db_manager.scoped_session_dependency)
+) -> DishORM:
+    return await crud.get_dish(session=session, menu_id=menu_id, submenu_id=submenu_id, dish_id=dish_id)
+
+
+@router.post('/{menu_id}/submenus/{submenu_id}/dishes/', response_model=Dish, status_code=status.HTTP_201_CREATED)
+async def create_dish(
+        menu_id: uuid.UUID,
+        submenu_id: uuid.UUID,
+        new_dish_data: DishCreate,
+        session: AsyncSession = Depends(async_db_manager.scoped_session_dependency)
+) -> DishORM:
+    return await crud.create_dish(session=session, menu_id=menu_id, submenu_id=submenu_id, new_dish_data=new_dish_data)
+
+
+@router.patch(
+    '/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}',
+    response_model=Dish,
+    status_code=status.HTTP_200_OK
+)
+async def update_dish(
+        menu_id: uuid.UUID,
+        submenu_id: uuid.UUID,
+        dish_id: uuid.UUID,
+        update_data: DishPartUpdate,
+        session: AsyncSession = Depends(async_db_manager.scoped_session_dependency)
+) -> DishORM:
+    return await crud.update_dish(
+        session=session,
+        menu_id=menu_id,
+        sub_menu_id=submenu_id,
+        dish_id=dish_id,
+        update_data=update_data
+    )
+
+
+@router.delete('/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}', status_code=status.HTTP_200_OK)
+async def delete_dish(
+        menu_id: uuid.UUID,
+        submenu_id: uuid.UUID,
+        dish_id: uuid.UUID,
+        session: AsyncSession = Depends(async_db_manager.scoped_session_dependency)
+) -> dict:
+    return await crud.delete_dish(
+        session=session,
+        menu_id=menu_id,
+        sub_menu_id=submenu_id,
+        dish_id=dish_id
+    )
