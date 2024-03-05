2ff4e5ce54a08e0596bc8df86e5c5ed187ea8e23
vladislav <pikakip222@mail.ru>
2024-03-05 19:56:45 +0300

    add user router

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from api_v1.dish.router import router as dish_router
 from api_v1.menu.router import router as menu_router
 from api_v1.sub_menu.router import router as sub_menu_router
+from api_v1.user.router import router as user_router
 
 router = APIRouter()
 router.include_router(router=menu_router, prefix='/menus')
 router.include_router(router=sub_menu_router, prefix='/menus')
 router.include_router(router=dish_router, prefix='/menus')
+router.include_router(router=user_router, prefix='/user')
