39adc3e0909f8b7c7b84d8de9fa0a3bede2fd6f9
vladislav <pikakip222@mail.ru>
2024-03-05 20:03:42 +0300

    add class AuthJWT

--- src/config.py
+++ src/config.py
@@ -1,4 +1,17 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
+
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / 'jwt-private.pem'
+    public_key_path: Path = BASE_DIR / "certs" / 'jwt-public.pem'
+    algorithm: str = 'RS256'
+    expire_token_minutes: int = 15
 
 
 class Settings(BaseSettings):
@@ -7,6 +20,8 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_NAME: str
     DB_PASS: str
+
+    auth_jwt: AuthJWT = AuthJWT()
 
     @property
     def DATABASE_URL_asyncpg(self):
